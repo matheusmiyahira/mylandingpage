aec837d1aabf3291a1616d394f02b9155629817f
matheusmiyhira <matheusmiyahira@gmail.com>
2020-05-02 02:31:22 -0300

    First Navbar

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,26 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
+  burger.addEventListener("click", () => {
+    // Toggle Nav
+    nav.classList.toggle("nav-active");
+    //Animante Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = ` navLinkFade 0.5s ease forwards ${
+          index / 7 + 1.5
+        }s`;
+      }
+    });
+  });
+};
+
+const app = () => {
+  navSlide();
+};
+
+app();
